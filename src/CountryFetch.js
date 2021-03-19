d09bc2634b23bd8f917c1b0050036637df64593e
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-03-19 20:30:37 -0300

    Add dropdown menu filter

--- src/CountryFetch.js
+++ src/CountryFetch.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CountryCard from "./components/CountryCard";
+import "./components/CountryCard.css";
 
 export default function CountryFetch() {
   const [countries, setCountries] = useState([]);
@@ -30,23 +31,41 @@ export default function CountryFetch() {
     fetchCountry();
   }, []);
 
-  let updateCountries = countries.filter((i) => {
+  const updateCountries = countries.filter((i) => {
     return i.name.toLowerCase().includes(query);
   });
 
   return (
     <>
       <form onSubmit={fetchSearchCountry}>
-        <input placeholder="Search" value={query} onChange={handleChange} />
+        <div className="input-container">
+          <input
+            className="search-bar"
+            placeholder="Search for a country..."
+            value={query}
+            onChange={handleChange}
+          />
+          <i className="fas fa-search search-image"></i>
+        </div>
+        <select>
+          <option>Filter by Region</option>
+          <option>Africa</option>
+          <option>America</option>
+          <option>Asia</option>
+          <option>Europe</option>
+          <option>Oceania</option>
+        </select>
       </form>
-      {updateCountries.map((country) => {
-        const { numericCode } = country;
-        return (
-          <article key={numericCode}>
-            <CountryCard country={country} />
-          </article>
-        );
-      })}
+      <div className="countries-container">
+        {updateCountries.map((country) => {
+          const { numericCode } = country;
+          return (
+            <article key={numericCode}>
+              <CountryCard country={country} />
+            </article>
+          );
+        })}
+      </div>
     </>
   );
 }
